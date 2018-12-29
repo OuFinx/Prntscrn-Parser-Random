d0f7848581e9925dd3c2aea8851683f92342d973
OuFinx <finx@live.ru>
2018-12-29 14:52:29 +0200

    Update main.py
    Add check for folder

--- main.py
+++ main.py
@@ -12,16 +12,22 @@ noneWorking = [0, 11809]
 userhome = os.path.expanduser('~')
 desktop = userhome + '/Desktop/Pictures_From_PRNSTCRN/'
 
+if not os.path.exists(desktop):
+    os.makedirs(desktop)
+
+
 def get_html(code):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     response = requests.get('https://prnt.sc/' + code, headers=headers, timeout=5)
     return response.text
+
 
 def RandomNumber(col, size=6, chars=string.ascii_lowercase + string.digits):
     code = ''
     for a in range(col):
         code = ''.join(random.choice(chars) for _ in range(size))
     return code
+
 
 def main(col):
     while True:
@@ -57,6 +63,7 @@ def main(col):
                     print("[+] Valid: " + str(code))
         except TypeError:
             print("ERROR - TypeError -" + str(code))
+
 
 if __name__ == '__main__':
     pool = ThreadPool(5)
